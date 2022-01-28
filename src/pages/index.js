ce97598810bb8e2ed06c29d87482b9772202f0fa
Chuck Knowles <cknowles1@gmail.com>
2022-01-28 16:20:45 -0500

    added some plugins and a local image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import * as React from "react";
 import Layout from "../component/layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby tutorial.</p>
+      <StaticImage
+        alt="Clifford, a reddish brown pit"
+        src="../images/navy-logo.svg.png"
+      />
     </Layout>
   );
 };
