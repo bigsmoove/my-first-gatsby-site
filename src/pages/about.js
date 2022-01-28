6e005c2fd7dfd25db5411994c44e342bf01cb761
Chuck Knowles <cknowles1@gmail.com>
2022-01-28 15:48:18 -0500

    finished gatsby tutorial 2

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+import Layout from "../component/layout";
+
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="About Me">
+      <p>Hi there. I'm the dude who made this demo page. Built with Gatsby.</p>
+    </Layout>
+  );
+};
+
+export default AboutPage;
